f59d004493d362fd67727f9ebdff5f70e3045240
charitha09 <charitha009@gmail.com>
2016-05-29 02:36:34 +0530

    no message

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,20 +1,20 @@
 <?php 
-	class Pages extends CI_Controller {  
-	
-		public function index() {
-			$page = 'home';
-			if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
-				show_404();//if requested page not exsists
-			}else{				
-				$data['title'] = ucfirst($page);//Capitalize the first letter for title
-				//Loarding DOM
-				$this->load->view('templates/html_header.php', $data);
-				$this->load->view('pages/'.$page, $data);				
-			}
-		} 
-  
-		public function hello() { 
-			echo "This is hello function."; 
-		} 
-	} 
+class Pages extends CI_Controller {  
+
+    public function index() {
+        $page = 'home';
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')){
+            show_404();//if requested page not exsists
+        }else{				
+            $data['title'] = ucfirst($page);//Capitalize the first letter for title
+            //Loarding DOM
+            $this->load->view('templates/html_header.php', $data);
+            $this->load->view('pages/'.$page, $data);				
+        }
+    } 
+
+    public function hello() { 
+            echo "This is hello function."; 
+    } 
+} 
 ?>
--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -1,8 +1,29 @@
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/home.css">
-	</head>	
-	<body> 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<script src="<?php echo base_url(); ?>public/js/common_script.js"></script>
-	</body>			
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/home.css">
+</head>	
+<body> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/js/common_script.js"></script>
+    <header id="header">  
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>public/images/header/logo.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>    
+    <div class="container">
+        <section id="cover">
+            <div class="col-sm-4">
+                <?php echo anchor('Pages/index/', 'HOME', 'class="col-sm-8 nav-property"') ?>              
+            </div>
+            <div class="col-sm-8">
+                
+            </div>
+        </section>
+    </div>
+</body>			
 </html>
