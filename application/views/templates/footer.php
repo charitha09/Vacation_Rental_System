17bb1bd17a8ca072ae252cb704777b5086be3dac
charitha09 <charitha009@gmail.com>
2016-06-01 00:18:36 +0530

    Footer Complete

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,25 @@
 <footer id="footer">
     <div class="col-sm-12 info">
-        <div class="col-sm-8">
-            
+        <div class="col-sm-8 contact">
+            <div class="col-sm-12">
+                <div class="col-sm-6">
+                    <a><i class="fa fa-phone" aria-hidden="true"></i><b> Tel :- </b>+94714525488</a>
+                </div>
+                <div class="col-sm-6">
+                    <a><i class="fa fa-envelope-o" aria-hidden="true"></i><b> Email :- </b>charitha009@gmail.com</a>
+                </div>
+            </div>
+            <div class="col-sm-12">
+                <div class="col-sm-6">
+                    <a><i class="fa fa-fax" aria-hidden="true"></i><b> Fax :- </b>+94412221703</a>
+                </div>
+                <div class="col-sm-6">
+                    <a><i class="fa fa-map-marker" aria-hidden="true"></i><b> Address :- </b>Punchchihewagewatta, Godagama, Matara.</a>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <div class="col-sm-1"></div>
+        <div class="col-sm-4 social">
+            <div class="col-sm-1 seperator-line"></div>
             <a href="#" class="col-sm-2 btn btn-social-icon btn-tripadvisor">
                 <span class="fa fa-tripadvisor"></span>
             </a>            
@@ -24,10 +39,10 @@
         </div>
     </div>
     <div class="col-sm-12 copyrights">
-        <div class="col-sm-8">
+        <div class="col-sm-6">
             <p>Copyright ©2013 Charitha Sampath All Rights Reserved</p>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-6">
             <p>Website Design, Develop & Hosting by Charitha Sampath  </p>
         </div>       
     </div>
